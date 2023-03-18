f291a59469782982e3f8b2545f6eeed4da25e83c
king-open <kingwang.0624@gmail.com>
2023-03-18 20:41:14 +0800

    复杂一点的高亮

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -52,9 +52,7 @@ export default{
 </script>
 
 <style lang="scss" scoped>
-.router-link-active {
-  text-decoration: underline;
-}
+
 .layout {
   display: flex;
   flex-direction: column;
@@ -85,7 +83,7 @@ export default{
 aside {
   background: lightblue;
   width: 150px;
-  padding: 16px;
+  padding: 16px 0;
   position: fixed;
   top: 0;
   left: 0;
@@ -93,10 +91,18 @@ aside {
   height: 100%;
   > h2 {
     margin-bottom: 4px;
+    padding:  0 16px;
   }
   > ol {
     > li {
-      padding: 4px 0;
+      >a {
+        display: block;
+        padding: 4px 16px;
+        text-decoration: none;
+      }
+      .router-link-active {
+        background: white;
+      }
     }
   }
   main{
