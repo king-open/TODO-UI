31b8ef3a1d27c60620f2b3537c1467323164646c
king-open <kingwang.0624@gmail.com>
2023-03-18 18:31:46 +0800

    switch css 2

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,53 +1,40 @@
 <template>
-  <div>
-    <button @click="toggle" :class="{checked:value}">
-      <span></span>
-    </button>
-  </div>
+  <button class="l-switch" @click="toggle" :class="{'l-checked':value}">
+    <span></span>
+  </button>
 </template>
-
 <script lang="ts">
-  export default {
-    props:{
-      value:Boolean,
-    },
-    setup(props,context){
-      const toggle = ()=> {
-        context.emit('update:input',!props.value)
-      }
-      return {toggle}
-    }
+import { ref } from "vue";
+export default {
+  props: {
+    value: Boolean,
+  },
+  setup(props, context) {
+    const toggle = () => {
+      context.emit("update:value", !props.value);
+    };
+    return { toggle };
   }
+};
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
-  button{
-    height: $h;
-    width: $h*2;
-    border: none;
-    background: gray;
-    border-radius: $h/2;
-    position: relative;
+.l-switch {
+  height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
+  > span {
+    position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
   }
-  span{
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
-    background:white;
-    border-radius: $h2 / 2;
-    transition: left 250ms;
+  &.l-checked { background: #1890ff;
+    > span { left: calc(100% - #{$h2} - 2px); }
   }
-  button.checked{
-    background: #6495ed;
+  &:focus { outline: none; }
+  &:active {
+    > span { width: $h2 + 4px; }
   }
-  button.checked > span {
-    left: calc(100% - #{$h2} - 2px);
+  &.l-checked:active {
+    > span { width: $h2 + 4px; margin-left: -4px; }
   }
-  button:focus{
-    outline: none;
-  }
+}
 </style>
