1bd03f34dd36018e35d5e6ceb3c37e2c178b85fe
king-open <kingwang.0624@gmail.com>
2023-03-18 19:19:35 +0800

    Dialog 支持 visible

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,23 +1,34 @@
 <template>
+  <template v-if="visible">
   <div class="l-dialog-overlay"></div>
-<div class="l-dialog-wrapper">
-  <div class="l-dialog">
-    <header>标题 <span class="l-dialog-close"></span></header>
-    <main>
-      <p>第一行字</p>
-      <p>第二行字</p>
-    </main>
-    <footer>
-      <Button level="main">OK</Button>
-      <Button>Cancel</Button>
-    </footer>
+  <div class="l-dialog-wrapper">
+    <div class="l-dialog">
+      <header>
+        标题
+        <span class="l-dialog-close"></span>
+      </header>
+      <main>
+        <p>第一行字</p>
+        <p>第二行字</p>
+      </main>
+      <footer>
+        <Button level="main">OK</Button>
+        <Button>Cancel</Button>
+      </footer>
+    </div>
   </div>
-</div>
+</template>
   </template>
   
 <script lang="ts">
   import Button from './Button.vue'
   export default {
+    props: {
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
     components: {
       Button
     }
