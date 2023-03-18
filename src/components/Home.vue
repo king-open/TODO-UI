51bf20fd683e404e5b72dabbc7ea9e9375080c97
king-open <kingwang.0624@gmail.com>
2023-03-18 16:06:18 +0800

    Topnav 组件封装好了

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <div class="topnav">
-      <div class="logo">listUI</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-    </div>
+    <Topnav/>
     <div class="banner">
       <h1>listUI</h1>
       <h2>一个厉害的 UI 框架</h2>
@@ -18,22 +12,11 @@
   </div>
 </template>
 
-<style lang="scss" scoped>
-.topnav {
-  background: lightpink;
-  display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
-    }
-  }
+<script lang="ts">
+import Topnav from '../components/Topnav.vue'
+export default{
+  components:{Topnav}
 }
-</style>
+</script>
+
+
