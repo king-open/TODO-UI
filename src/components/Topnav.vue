439043e59a8cbc39e5578f03b620d4ccffd6f169
king-open <kingwang.0624@gmail.com>
2023-03-18 20:35:02 +0800

    更换切换按钮的样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -9,12 +9,20 @@
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
-    <span class="toggleAside" @click="toggleMenu"></span>
+    <svg v-if="toggleMenuButtonVisible" class="toggleAside" @click="toggleMenu">
+    <use xlink:href="#icon-menu"></use>
+  </svg>
   </div>
 </template>
 <script lang="ts">
 import { inject, Ref } from 'vue'
 export default {
+  props: {
+    toggleMenuButtonVisible: {
+      type: Boolean,
+      default: false
+    }
+  },
   setup(){
     const menuVisible = inject<Ref<boolean>>('menuVisible'); // get
     const toggleMenu = ()=>{
@@ -53,14 +61,14 @@ $color: #007974;
     }
   }
   > .toggleAside{
-    width: 24px;
-    height: 24px;
-    background: red;
+    width: 32px;
+    height: 32px;
     position: absolute;
     left: 16px;
     top: 50%;
     transform: translateY(-50%);
     display: none;
+    background: fade-out(black, 0.9);
   }
   @media (max-width: 500px) {
     > .menu{display: none;}
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="layout">
-    <Topnav class="nav"/>
-    <div class="content">
-      <aside  v-if="menuVisible">
-        <h2>组件列表</h2>
-        <ol>
-          <li>
-            <router-link to="/doc/switch">Switch 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/button">Button 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/dialog">Dialog 组件</router-link>
-          </li>
-          <li>
-            <router-link to="/doc/tabs">Tabs 组件</router-link>
-          </li>
-        </ol>
-      </aside>
-      <main>
-        <router-view/>
-      </main>
-    </div>
+  <Topnav toggleMenuButtonVisible class="nav" />
+  <div class="content">
+    <aside v-if="menuVisible">
+      <h2>组件列表</h2>
+      <ol>
+        <li>
+          <router-link to="/doc/switch">Switch 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/button">Button 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/dialog">Dialog 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/tabs">Tabs 组件</router-link>
+        </li>
+      </ol>
+    </aside>
+    <main>
+      <router-view />
+    </main>
   </div>
+</div>
 </template>
 
 <script lang="ts">
