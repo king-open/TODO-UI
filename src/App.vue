e9b6a34bacd2535e724202b55df3ff76a221728b
king-open <kingwang.0624@gmail.com>
2023-03-18 17:11:20 +0800

    完善 aside 切换逻辑

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,13 @@
 </template>
 
 <script lang="ts">
-import { provide, ref } from 'vue'
+import { provide, ref } from 'vue';
 
 export default {
   name: 'App',
   setup(){
-    const menuVisible = ref(false)
+    const width = document.documentElement.clientWidth
+    const menuVisible = ref(width <= 500 ? false :true);
     provide('menuVisible',menuVisible)
   }
 }
