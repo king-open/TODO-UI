b1f44fedb1186fe576874f35be1b641957dce8ed
king-open <kingwang.0624@gmail.com>
2023-03-18 18:10:16 +0800

    Switch css 1

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -13,5 +13,23 @@
 </script>
 
 <style lang="scss" scoped>
-
+$h: 22px;
+$h2: $h - 4px;
+  button{
+    height: $h;
+    width: $h*2;
+    border: none;
+    background: #6495ed;
+    border-radius: $h/2;
+    position: relative;
+  }
+  span{
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background:white;
+    border-radius: $h2 / 2;
+  }
 </style>
