63ba4071883a1156526ceab3d86e102be064dca4
king-open <kingwang.0624@gmail.com>
2023-03-18 21:07:38 +0800

    事不过三：优化 markdown

---
+++ src/markdown/install.md
@@ -0,0 +1,9 @@
+# 安装
+
+打开终端运行下列命令：
+
+```
+npm install list-ui
+```
+
+下一节：[开始使用](#/doc/get-started)
---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+// @ts-nocheck
+
+
+
+
+import { md } from "./plugins/md";
+
+export default {
+  plugins: [md()]
+};
