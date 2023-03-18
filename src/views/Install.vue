6cb3133edbcf138dda02304f6f6c5bf912cbb7a1
king-open <kingwang.0624@gmail.com>
2023-03-18 20:43:51 +0800

    填充文档内容

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    安装
-  </div>
-  </template>
+  <article>
+    <h1>安装</h1>
+    <p>打开终端运行下列命令：</p>
+    <pre><code>npm install list-ui</code></pre>
+
+    <p>下一节：<a href="#/doc/get-started">开始使用</a></p>
+  </article>
+</template>
