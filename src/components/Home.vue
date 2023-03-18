2c257a93a239da928218a12a4d8aba6110fbbe5e
king-open <kingwang.0624@gmail.com>
2023-03-18 16:54:27 +0800

    初始化 aside 的 html

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,8 +5,8 @@
       <h1>listUI</h1>
       <h2>一个厉害的 UI 框架</h2>
       <p class="actions">
-        <a>GitHub</a>
-        <a>开始</a>
+        <a href="https://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
