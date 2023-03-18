8e73840340a2244532d80cbfe14e0387979cf704
king-open <kingwang.0624@gmail.com>
2023-03-18 17:56:30 +0800

    Switch 组件添加 html

---
+++ src/lib/Switch.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button>
+      <span></span>
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
